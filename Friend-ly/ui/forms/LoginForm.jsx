7c70261743253f26c6bcf851c8cb65c844e02fba
Slys <slysdeveloper@gmail.com>
2025-01-05 17:07:25 -0800

    feat(auth): integrate Microsoft login with popup authentication in LoginView; update LoginForm to handle submission

--- Friend-ly/ui/forms/LoginForm.jsx
+++ Friend-ly/ui/forms/LoginForm.jsx
@@ -4,9 +4,11 @@ import { View, StyleSheet, Alert } from 'react-native';
 import InputField from '../components/InputField';
 import Button from '../components/PrimaryButton';
 
-const LoginForm = () => {
+const LoginForm = ({ onSubmit }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+
 
   const handleSubmit = () => {
     if (!email || !password) {
@@ -18,7 +20,7 @@ const LoginForm = () => {
 
   return (
     <View style={styles.container}>
-      <InputField
+      {/* <InputField
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
@@ -28,8 +30,8 @@ const LoginForm = () => {
         value={password}
         onChangeText={setPassword}
         secureTextEntry
-      />
-      <Button text="Login" onPress={handleSubmit} />
+      /> */}
+      <Button text="Login" onPress={onSubmit} />
     </View>
   );
 };
--- Friend-ly/ui/screens/LoginView.jsx
+++ Friend-ly/ui/screens/LoginView.jsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import LoginForm from '../forms/LoginForm';
+import { GoogleAuthProvider } from "firebase/auth"
+import { signInWithPopup } from "firebase/auth"
+import { auth } from '../../server/firebase/firebase';
 
 const LoginView = () => {
+    
+   const microsoftProvider = new GoogleAuthProvider()
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      });
+    
+    /* 
+      Creates a popup for the user to login. 
+      Done through Microsoft, server will check if
+      the user is using a uw.edu email. 
+    */
+    const authLogin = async () => {
+      try {
+        const results = await signInWithPopup(auth, microsoftProvider)
+        try {
+          const response = await fetch('http://localhost:6262/api/auth', {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ token: results.user.accessToken }),
+          });
+          const data = await response.json();
+          console.log('Auth response:', data);
+          return data;
+      } catch (err) {
+          console.error('Error during authentication:', err);
+      }
+      // TODO: Do we need to do anything if server is authenticated. 
+      } catch (err) {
+        throw (err)
+      }
+      console.log("HI")
+    }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome Back!</Text>
-      <LoginForm />
+      <LoginForm onSubmit={authLogin}/>
     </View>
   );
 };
